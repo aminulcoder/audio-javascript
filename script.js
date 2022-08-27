117e85da645bf7c7745cf4eab86b4b5a8f721433
aminulcoder <aminulislammd97@gmail.com>
2022-08-27 21:09:24 +0600

    javascript audio play

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+// for(var i=0; i<3; i++){
+
+//     document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+
+//         var text = this.innerHTML;
+//         document.querySelector("h1").innerHTML = text + "is clicked";
+    
+//     });
+
+// }
+
+
+for(var i=0; i<3; i++){
+
+
+    document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+        var text = this.innerHTML;
+        console.log(text);
+        audioPlay(text);
+
+
+        
+    });
+
+
+
+    function audioPlay(text){
+
+
+        switch(text){
+
+            case "a":
+                var audio = new Audio("/sounds/a.mp3");
+                audio.play();
+                break;
+            case "b":
+                var audio = new Audio("/sounds/b.mp3");
+                audio.play();
+                break;
+            case "c":
+                var audio = new Audio("/sounds/c.mp3");
+                audio.play();
+                break;
+    
+        }
+
+    }
+  
+}
